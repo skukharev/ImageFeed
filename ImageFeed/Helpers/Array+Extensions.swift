3c45b757544c1c6a0eb0ea1a8439b05c4e40e8c0
Sergej Kukharev <kukharev@narod.ru>
2024-06-12 16:27:58 +0300

    Обновление расширений из общей библиотеки расширений

--- ImageFeed/Helpers/Array+Extensions.swift
+++ ImageFeed/Helpers/Array+Extensions.swift
@@ -6,6 +6,14 @@
 
 import Foundation
 
+/*
+    Расширение для безопасного обращения к элементам массивов по индексу
+ 
+    Использование:
+        let arr: [String] = ["1", "2"]
+        print(arr[safe: 3]) //Вернёт nil, а не ошибку времени исполнения
+*/
+
 extension Array {
     subscript(safe index: Index) -> Element? {
         indices ~= index ? self[index] : nil
